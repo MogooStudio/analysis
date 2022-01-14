cf7745b9e878cca922b3817e6c32eb5a5cbb9861
jinshan <jinshan@hummergames.com>
2022-01-14 21:14:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 import baostock as bs
 import akshare as ak
 import pandas as pd
@@ -13,79 +15,89 @@ e_threshold = {  # 低估值指标阈值
     e_pb: 0.5,
     e_value: 100 * unit,
 }
-STR_DAY = (datetime.date.today() + datetime.timedelta(days=-1)).strftime('%Y-%m-%d')  # 查询日期
-print(STR_DAY)
+
+today = datetime.date.today()
+delta = 0
+if today.weekday() == 6:
+    delta = -2
+elif today.weekday() == 0:
+    delta = -3
+day = today + datetime.timedelta(days=delta)
+STR_DAY = day.strftime('%Y-%m-%d')  # 查询日期
+print("{0}|{1};{2}|{3}".format(today, today.weekday(), STR_DAY, day.weekday()))
 
 
 stock_all = {  # 股票数据
         # wx
-        "002714": {"name": "牧原股份", "class": "sz", "valid": True, "type": e_value, "low": 2500*unit, "high": 6000*unit},
-        "600036": {"name": "招商银行", "class": "sh", "valid": True, "type": e_pb, "low": 1.5, "high": 5},
-        "603899": {"name": "晨光文具", "class": "sh", "valid": True, "type": e_pe, "low": 32, "high": h_value},
-        "601100": {"name": "恒立液压", "class": "sh", "valid": True, "type": e_pe, "low": 34, "high": h_value},
-        "603195": {"name": "公牛集团", "class": "sh", "valid": True, "type": e_pe, "low": 30, "high": h_value},
-        "601636": {"name": "旗滨集团", "class": "sh", "valid": True, "type": e_pe, "low": 9, "high": h_value},
-        "600276": {"name": "恒瑞医药", "class": "sh", "valid": True, "type": e_pe, "low": 50, "high": h_value},
-        "000661": {"name": "长春高新", "class": "sz", "valid": True, "type": e_pe, "low": 30, "high": h_value},
-        "002230": {"name": "科大讯飞", "class": "sz", "valid": True, "type": e_pe, "low": 60, "high": h_value},
-        "300999": {"name": "金龙鱼", "class": "sz", "valid": False, "type": e_pe, "low": 50, "high": h_value},
-        "601888": {"name": "中国中免", "class": "sh", "valid": True, "type": e_pe, "low": 30, "high": h_value},
-        "300759": {"name": "康龙化成", "class": "sz", "valid": False, "type": e_pe, "low": 60, "high": h_value},
-        "002241": {"name": "歌尔股份", "class": "sz", "valid": True, "type": e_pe, "low": 35, "high": h_value},
-        "300347": {"name": "泰格医药", "class": "sz", "valid": False, "type": e_pe, "low": 48, "high": h_value},
-        "300274": {"name": "阳光电源", "class": "sz", "valid": False, "type": e_pe, "low": 34, "high": h_value},
-        "600887": {"name": "伊利股份", "class": "sh", "valid": True, "type": e_pe, "low": 25, "high": h_value},
-        "603486": {"name": "科沃斯", "class": "sh", "valid": True, "type": e_pe, "low": 40, "high": h_value},
-        "002475": {"name": "立讯精密", "class": "sz", "valid": True, "type": e_pe, "low": 32, "high": h_value},
-        "002415": {"name": "海康威视", "class": "sz", "valid": True, "type": e_pe, "low": 24, "high": h_value},
-        "603259": {"name": "药明康德", "class": "sh", "valid": True, "type": e_pe, "low": 60, "high": h_value},
-        "000333": {"name": "美的集团", "class": "sz", "valid": True, "type": e_pe, "low": 15, "high": 25},
-        "600438": {"name": "通威股份", "class": "sh", "valid": True, "type": e_pe, "low": 20, "high": h_value},
-        "002027": {"name": "分众传媒", "class": "sz", "valid": True, "type": e_pe, "low": 17, "high": h_value},
-        "601012": {"name": "隆基股份", "class": "sh", "valid": True, "type": e_pe, "low": 20, "high": h_value},
-        "300015": {"name": "爱尔眼科", "class": "sz", "valid": True, "type": e_pe, "low": 70, "high": h_value},
-        "601155": {"name": "新城控股", "class": "sh", "valid": True, "type": e_pe, "low": 4.5, "high": h_value},
-        "600298": {"name": "安琪酵母", "class": "sh", "valid": True, "type": e_pe, "low": 0, "high": h_value},
-        "002352": {"name": "顺丰控股", "class": "sz", "valid": True, "type": e_pe, "low": 30, "high": h_value},
-        "601877": {"name": "正泰电器", "class": "sh", "valid": True, "type": e_pe, "low": 14, "high": h_value},
-        "000858": {"name": "五粮液", "class": "sz", "valid": True, "type": e_pe, "low": 28, "high": h_value},
-        "601225": {"name": "陕西煤业", "class": "sh", "valid": True, "type": e_pe, "low": 6.7, "high": h_value},
-        "601615": {"name": "明阳智能", "class": "sh", "valid": True, "type": e_pe, "low": 18, "high": h_value},
-        "002271": {"name": "东方雨虹", "class": "sz", "valid": True, "type": e_pe, "low": 20, "high": h_value},
-        "600085": {"name": "同仁堂", "class": "sh", "valid": True, "type": e_pe, "low": 36, "high": h_value},
-        "01810": {"name": "小米集团", "class": "hk", "valid": True, "type": e_pe, "low": 12, "high": h_value},
-        "002304": {"name": "洋河股份", "class": "sz", "valid": True, "type": e_pe, "low": 24, "high": h_value},
-        "000538": {"name": "云南白药", "class": "sz", "valid": True, "type": e_pe, "low": 24, "high": h_value},
-        "603369": {"name": "今世缘", "class": "sh", "valid": True, "type": e_pe, "low": 23, "high": h_value},
-        "600332": {"name": "白云山", "class": "sh", "valid": True, "type": e_pe, "low": 14, "high": h_value},
-        "300595": {"name": "欧普康视", "class": "sz", "valid": True, "type": e_pe, "low": 48, "high": h_value},
-        "600905": {"name": "三峡能源", "class": "sh", "valid": True, "type": e_pe, "low": 35, "high": h_value},
-        "603127": {"name": "昭衍新药", "class": "sh", "valid": True, "type": e_pe, "low": 60, "high": h_value},
+        "002714": {"name": "牧原股份", "class": "sz", "type": e_value, "low": 2500*unit, "high": 6000*unit},
+        "600036": {"name": "招商银行", "class": "sh", "type": e_pb, "low": 1.5, "high": 5},
+        "603899": {"name": "晨光文具", "class": "sh", "type": e_pe, "low": 32, "high": h_value},
+        "601100": {"name": "恒立液压", "class": "sh", "type": e_pe, "low": 34, "high": h_value},
+        "603195": {"name": "公牛集团", "class": "sh", "type": e_pe, "low": 30, "high": h_value},
+        "601636": {"name": "旗滨集团", "class": "sh", "type": e_pe, "low": 9, "high": h_value},
+        "600276": {"name": "恒瑞医药", "class": "sh", "type": e_pe, "low": 50, "high": h_value},
+        "000661": {"name": "长春高新", "class": "sz", "type": e_pe, "low": 30, "high": h_value},
+        "002230": {"name": "科大讯飞", "class": "sz", "type": e_pe, "low": 60, "high": h_value},
+        "300999": {"name": "金龙鱼", "class": "sz", "type": e_pe, "low": 50, "high": h_value},
+        "601888": {"name": "中国中免", "class": "sh", "type": e_pe, "low": 30, "high": h_value},
+        "300759": {"name": "康龙化成", "class": "sz", "type": e_pe, "low": 60, "high": h_value},
+        "002241": {"name": "歌尔股份", "class": "sz", "type": e_pe, "low": 35, "high": h_value},
+        "300347": {"name": "泰格医药", "class": "sz", "type": e_pe, "low": 48, "high": h_value},
+        "300274": {"name": "阳光电源", "class": "sz", "type": e_pe, "low": 34, "high": h_value},
+        "600887": {"name": "伊利股份", "class": "sh", "type": e_pe, "low": 25, "high": 35},
+        "603486": {"name": "科沃斯", "class": "sh", "type": e_pe, "low": 40, "high": h_value},
+        "002475": {"name": "立讯精密", "class": "sz", "type": e_pe, "low": 32, "high": h_value},
+        "002415": {"name": "海康威视", "class": "sz", "type": e_pe, "low": 24, "high": h_value},
+        "603259": {"name": "药明康德", "class": "sh", "type": e_pe, "low": 60, "high": h_value},
+        "000333": {"name": "美的集团", "class": "sz", "type": e_pe, "low": 15, "high": 25},
+        "600438": {"name": "通威股份", "class": "sh", "type": e_pe, "low": 20, "high": h_value},
+        "002027": {"name": "分众传媒", "class": "sz", "type": e_pe, "low": 17, "high": h_value},
+        "601012": {"name": "隆基股份", "class": "sh", "type": e_pe, "low": 20, "high": h_value},
+        "300015": {"name": "爱尔眼科", "class": "sz", "type": e_pe, "low": 70, "high": h_value},
+        "601155": {"name": "新城控股", "class": "sh", "type": e_pe, "low": 4.5, "high": h_value},
+        "600298": {"name": "安琪酵母", "class": "sh", "type": e_pe, "low": 0, "high": h_value},
+        "002352": {"name": "顺丰控股", "class": "sz", "type": e_pe, "low": 30, "high": h_value},
+        "601877": {"name": "正泰电器", "class": "sh", "type": e_pe, "low": 14, "high": h_value},
+        "000858": {"name": "五粮液", "class": "sz", "type": e_pe, "low": 28, "high": h_value},
+        "601225": {"name": "陕西煤业", "class": "sh", "type": e_pe, "low": 6.7, "high": h_value},
+        "601615": {"name": "明阳智能", "class": "sh", "type": e_pe, "low": 18, "high": h_value},
+        "002271": {"name": "东方雨虹", "class": "sz", "type": e_pe, "low": 20, "high": h_value},
+        "600085": {"name": "同仁堂", "class": "sh", "type": e_pe, "low": 36, "high": h_value},
+        "002304": {"name": "洋河股份", "class": "sz", "type": e_pe, "low": 24, "high": h_value},
+        "000538": {"name": "云南白药", "class": "sz", "type": e_pe, "low": 24, "high": h_value},
+        "603369": {"name": "今世缘", "class": "sh", "type": e_pe, "low": 23, "high": h_value},
+        "600332": {"name": "白云山", "class": "sh", "type": e_pe, "low": 14, "high": h_value},
+        "300595": {"name": "欧普康视", "class": "sz", "type": e_pe, "low": 48, "high": h_value},
+        "600905": {"name": "三峡能源", "class": "sh", "type": e_pe, "low": 35, "high": h_value},
+        "603127": {"name": "昭衍新药", "class": "sh", "type": e_pe, "low": 60, "high": h_value},
+        "601166": {"name": "兴业银行", "class": "sh", "type": e_pb, "low": 0.75, "high": h_value},
+        "600989": {"name": "宝丰能源", "class": "sh", "type": e_pe, "low": 16, "high": h_value},
+        "600383": {"name": "金地集团", "class": "sh", "type": e_pe, "low": 6, "high": h_value},
         # self
-        "002597": {"name": "金禾实业", "class": "sz", "valid": True, "type": e_pe, "low": 18, "high": h_value},
-        # "002698": {"name": "博实股份", "class": "sz", "valid": True, "type": e_pe, "low": 18, "high": h_value},
-        # "002594": {"name": "比亚迪", "class": "sz", "valid": True, "type": e_pe, "low": 18, "high": h_value},
-        # "603236": {"name": "移远通信", "class": "sh", "valid": True, "type": e_pe, "low": 18, "high": h_value},
-        # "002791": {"name": "坚朗五金", "class": "sz", "valid": True, "type": e_pe, "low": 18, "high": h_value},
-        # "603986": {"name": "兆易创新", "class": "sh", "valid": True, "type": e_pe, "low": 18, "high": h_value},
-        # "603833": {"name": "欧派家居", "class": "sh", "valid": True, "type": e_pe, "low": 18, "high": h_value},
-        # "002920": {"name": "德赛西威", "class": "sz", "valid": True, "type": e_pe, "low": 18, "high": h_value},
-        # "600745": {"name": "闻泰科技", "class": "sh", "valid": True, "type": e_pe, "low": 18, "high": h_value},
-        # "603288": {"name": "海天味业", "class": "sh", "valid": True, "type": e_pe, "low": 18, "high": h_value},
-        # "000538": {"name": "云南白药", "class": "sz", "valid": True, "type": e_pe, "low": 18, "high": h_value},
-        # "600600": {"name": "青岛啤酒", "class": "sh", "valid": True, "type": e_pe, "low": 18, "high": h_value},
-        # "002311": {"name": "海大集团", "class": "sz", "valid": True, "type": e_pe, "low": 18, "high": h_value},
-        # "600885": {"name": "宏发股份", "class": "sh", "valid": True, "type": e_pe, "low": 18, "high": h_value},
-        # "002410": {"name": "广联达", "class": "sz", "valid": True, "type": e_pe, "low": 18, "high": h_value},
-        # "002032": {"name": "苏泊尔", "class": "sz", "valid": True, "type": e_pe, "low": 18, "high": h_value},
-        # "600845": {"name": "宝信软件", "class": "sh", "valid": True, "type": e_pe, "low": 18, "high": h_value},
-        # "002557": {"name": "洽洽食品", "class": "sz", "valid": True, "type": e_pe, "low": 18, "high": h_value},
-        # "600754": {"name": "锦江酒店", "class": "sh", "valid": True, "type": e_pe, "low": 18, "high": h_value},
-        # "603939": {"name": "益丰药房", "class": "sh", "valid": True, "type": e_pe, "low": 18, "high": h_value},
-        # "000049": {"name": "德赛电池", "class": "sz", "valid": True, "type": e_pe, "low": 18, "high": h_value},
-        # "600085": {"name": "同仁堂", "class": "sh", "valid": True, "type": e_pe, "low": 18, "high": h_value},
-        # "603658": {"name": "安图生物", "class": "sh", "valid": True, "type": e_pe, "low": 18, "high": h_value},
-        # "603387": {"name": "基蛋生物", "class": "sh", "valid": True, "type": e_pe, "low": 18, "high": h_value},
+        "002597": {"name": "金禾实业", "class": "sz", "type": e_pe, "low": 18, "high": h_value},
+        "000963": {"name": "华东医药", "class": "sz", "type": e_pe, "low": 20, "high": h_value},
+        "002594": {"name": "比亚迪", "class": "sz", "type": e_pe, "low": 155, "high": h_value},
+        "603236": {"name": "移远通信", "class": "sh", "type": e_pe, "low": 92, "high": h_value},
+        "002791": {"name": "坚朗五金", "class": "sz", "type": e_pe, "low": 34, "high": h_value},
+        # "603986": {"name": "兆易创新", "class": "sh", "type": e_pe, "low": 57, "high": h_value},
+        # "603833": {"name": "欧派家居", "class": "sh", "type": e_pe, "low": 18, "high": h_value},
+        # "002920": {"name": "德赛西威", "class": "sz", "type": e_pe, "low": 18, "high": h_value},
+        # "600745": {"name": "闻泰科技", "class": "sh", "type": e_pe, "low": 18, "high": h_value},
+        # "603288": {"name": "海天味业", "class": "sh", "type": e_pe, "low": 18, "high": h_value},
+        # "000538": {"name": "云南白药", "class": "sz", "type": e_pe, "low": 18, "high": h_value},
+        # "600600": {"name": "青岛啤酒", "class": "sh", "type": e_pe, "low": 18, "high": h_value},
+        # "002311": {"name": "海大集团", "class": "sz", "type": e_pe, "low": 18, "high": h_value},
+        # "600885": {"name": "宏发股份", "class": "sh", "type": e_pe, "low": 18, "high": h_value},
+        # "002410": {"name": "广联达", "class": "sz", "type": e_pe, "low": 18, "high": h_value},
+        # "002032": {"name": "苏泊尔", "class": "sz", "type": e_pe, "low": 18, "high": h_value},
+        # "600845": {"name": "宝信软件", "class": "sh", "type": e_pe, "low": 18, "high": h_value},
+        # "002557": {"name": "洽洽食品", "class": "sz", "type": e_pe, "low": 18, "high": h_value},
+        # "600754": {"name": "锦江酒店", "class": "sh", "type": e_pe, "low": 18, "high": h_value},
+        # "603939": {"name": "益丰药房", "class": "sh", "type": e_pe, "low": 18, "high": h_value},
+        # "000049": {"name": "德赛电池", "class": "sz", "type": e_pe, "low": 18, "high": h_value},
+        # "600085": {"name": "同仁堂", "class": "sh", "type": e_pe, "low": 18, "high": h_value},
+        # "603658": {"name": "安图生物", "class": "sh", "type": e_pe, "low": 18, "high": h_value},
+        # "603387": {"name": "基蛋生物", "class": "sh", "type": e_pe, "low": 18, "high": h_value},
     }
 
 
@@ -141,13 +153,10 @@ class Test:
             real_code = stock_code.split('.')[1]
             code_data = stock_all.get(real_code)
             code_name = code_data["name"]
-            valid = code_data["valid"]
             low_ttm = code_data["low"]
             high_ttm = code_data["high"]
             type_ttm = code_data["type"]
             value_low = e_threshold[type_ttm]
-            if not valid:
-                continue
             if type_ttm == e_pe:
                 real_ttm = stock_data[e_ttm_pe]
             elif type_ttm == e_pb:
@@ -156,7 +165,7 @@ class Test:
                 real_ttm = self.get_market(real_code)
             diff_low = float(real_ttm) - float(low_ttm)
             diff_high = float(real_ttm) - float(high_ttm)
-            if 0 < diff_low <= value_low:
+            if diff_low <= value_low:
                 low_code.append({
                     "name": code_name,
                     "code": stock_code,
@@ -173,14 +182,19 @@ class Test:
                     "ttm_diff": round(diff_high, 2),
                 })
 
+        def takeTTMDiff(elem):
+            return elem["ttm_diff"]
+
         print("########################################################################")
         print("#                           筛选低估值的股票                              #")
         print("########################################################################")
+        low_code.sort(key=takeTTMDiff)
         for data in low_code:
             print("\t股票:{0}\t代码:{1}\t当前值:{2}\t低估值:{3}\t还差:{4}".format(data["name"], data["code"], data["ttm_current"], data["ttm_low"], data["ttm_diff"]))
         print("########################################################################")
         print("#                           剔除估值高的股票                              #")
         print("########################################################################")
+        high_code.sort(reverse=True, key=takeTTMDiff)
         for data in high_code:
             print("\t股票:{0}\t代码:{1}\t当前值:{2}\t高估值:{3}\t高出:{4}".format(data["name"], data["code"], data["ttm_current"], data["ttm_high"], data["ttm_diff"]))
         print("########################################################################")
@@ -193,7 +207,7 @@ def baostock():
         print('login respond  error_msg:' + lg.error_msg)
     ts = Test(STR_DAY)
     ts.run()
-    ts.test("sz.002271")
+    # ts.test("sz.002271")
     bs.logout()
 
 
